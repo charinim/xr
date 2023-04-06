945173707f9d63cd2a18e36142888a3e4f50bfac
Harini Machiraju <116056136+charinim@users.noreply.github.com>
2023-04-06 12:58:00 -0700

    Update custom-models.js

--- src/Experience/World/custom-models.js
+++ src/Experience/World/custom-models.js
@@ -4,10 +4,10 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
 var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-cube.position.set( 2, 0, 0 );
+cube.position.set(10 , 0, 0 );
+scene.add(cube)
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
